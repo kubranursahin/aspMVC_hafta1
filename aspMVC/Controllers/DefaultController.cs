2731678ef31355a9a7cfee505f11b1dee550977d
Kübranur Şahin <78857206+kubranursahin@users.noreply.github.com>
2022-03-14 21:38:07 +0300

    Add project files.

---
+++ aspMVC/Controllers/DefaultController.cs
@@ -0,0 +1,37 @@
+﻿using aspMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aspMVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            DateTime zaman = DateTime.Now;
+            return View(zaman);
+        }
+        public ActionResult Liste()
+        {
+            int[] sayilar = new int[] { 1 , 2 , 3 , 4 , 5};
+            return View("List" , sayilar);
+        }
+        public ActionResult Dersim()
+        {
+            int id = 101;
+            string dersAdi = "Asp.Net 2 MVC Programlama";
+            string dersEgitmeni = "Fatma Betül ";
+            int dersKredi = 25;
+            Ders model = new Ders();
+            model.Id = id;
+            model.Ad = dersAdi;
+            model.Egitmen = dersEgitmeni;
+            model.Kredi = dersKredi;
+            return View(model);
+        }
+    }
+}
---
+++ aspMVC/Models/Ders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace aspMVC.Models
+{
+    public class Ders
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Egitmen { get; set; }
+        public int Kredi { get; set; }
+    }
+}
